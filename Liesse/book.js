24ac7ea0ee323498826ae68d7492f52416ccb2c7
Liesse Ishimwe <ishimwelieish@gmail.com>
2024-08-19 18:39:10 +0200

    SingleBook_Page_updated_and_dynamiskt

--- Liesse/book.js
+++ Liesse/book.js
@@ -1,49 +1,153 @@
-document.addEventListener('DOMContentLoaded', () => {
 const apiKey = 'AIzaSyB8d14nV9lif1OHMhgDG5eYB8SxjtGIqKI';
 const query = 'Harry Potter';
-
-fetch(`https://www.googleapis.com/books/v1/volumes?q=*&orderBy=relevance&maxResults=5&key=${apiKey}`)
-.then(response => response.json())
-.then(data => {
-// Process the data
-if (data.items && data.items.length > 0) {
-    data.items.forEach(book => {
-    const books = book;
-    displayBooks(books);
-    });
-
-}
-})
-.catch(error => console.error('Error fetching data:', error));
-
- function handleBookImageClick() {
-     // console.log(clickedElement)
-        let information = clickedElement.getAttribute("data-info");
-
-        console.log('Clicked on book:', information);
-        // For example, you can open a new page with more details or perform other actions
-      }
-
-function displayBooks(books) {
-    const bookList = document.getElementById("book-list");
-        const bookItem = document.createElement("div");
-        bookItem.classList.add("boxSectn");
-      bookItem.innerHTML = `
-            <img src="${books.volumeInfo.imageLinks.thumbnail}"
-                 alt="${books.volumeInfo.title} Cover"
-                 class="bookBox"
-                 onclick="handleBookImageClick(this)"
-            >
-            <p>${books.volumeInfo.title}</p>
-        `;
-        bookList.appendChild(bookItem);
-
-        
+const bookList = document.getElementById("book-list");
+const bookInfo = document.querySelector(".bookInfo");
+const urlParams = new URLSearchParams(window.location.search);
+const bookID = urlParams.get('id');
+if (bookID) {
+getSingleBookInfo(bookID);
 }
 
 
-// onclick="location.href='http://127.0.0.1:5500/Liesse/book.html';"
+//const booksData = await getBooks();
+//async function booksForm() {}
 
-    
 
-    });
+async function getBooks () {
+const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=*&orderBy=relevance&maxResults=5&key=${apiKey}`;
+const response =  await fetch(apiUrl);
+if (!response.ok) {
+throw new Error(`Error: ${response.status}`);
+}
+const books = await response.json();
+displayBooksInfo(books.items)
+}
+
+
+function displayBooksInfo (data) {
+data.forEach((book) => {
+//let bookID= book.id;
+const bookItem = document.createElement("div");
+bookItem.classList.add("boxSectn");
+bookItem.innerHTML = `
+<img src="${book.volumeInfo.imageLinks.thumbnail}"
+alt="${book.volumeInfo.title} Cover"
+>
+`;
+
+bookList.appendChild(bookItem);
+//Säkerställa att addEventListener sker after vi har boken objekt har skapas
+bookItem.addEventListener("click",() => {
+location.href=`http://127.0.0.1:5500/Liesse/book.html?id=${book.id}`;
+});
+
+});
+
+}
+
+
+async function getSingleBookInfo (bookID){
+const apiUrl = `https://www.googleapis.com/books/v1/volumes/${bookID}??key=${apiKey}`;
+const response =  await fetch(apiUrl);
+if (!response.ok) {
+throw new Error(`Error: ${response.status}`);
+}
+const singleBook = await response.json();
+//await new Promise(resolve => setTimeout(resolve, 1000));
+displaySingleBookInfo (singleBook.volumeInfo)
+};
+
+function displaySingleBookInfo(singleBook){
+const bookInfoItem = document.createElement("div");
+const categoriesHTML = singleBook.categories.map(categorie => `${categorie}`).join('<br>');
+const authorsHTML = singleBook.authors.map(author => `${author}`).join(',');
+
+console.log(singleBook)
+bookInfoItem.innerHTML = `
+<section class="div1">
+<div>
+<img src=${singleBook.imageLinks.small} alt="" width="" height="350px">
+<button class="btn_bookBuy" >Buy on Amazon</button>
+</div>
+<div class="textBok">
+<h1>${singleBook.title}</h1>
+<h4>${authorsHTML}</h4>
+<div id="rating-stars">
+<span class="star selected" onclick="rate(1)">&#9733;</span>
+<span class="star selected" onclick="rate(2)">&#9733;</span>
+<span class="star selected" onclick="rate(3)">&#9733;</span>
+<span class="star selected" onclick="rate(4)">&#9733;</span>
+<span class="star" onclick="rate(5)">&#9733;</span>
+
+</div>
+<p>${singleBook.description}</p>
+
+<p class="infoBok"> <strong>Genre:</strong><br>
+${categoriesHTML}
+</p>
+<p><strong>This edition</strong><br>
+Format: ${singleBook.pageCount} pages, ${singleBook.printType}<br>
+Published: ${singleBook.publishedDate} by ${singleBook.publisher} <br>
+Language: ${singleBook.language}<br>
+
+</p>
+</div>
+</section>
+
+
+<section class="rviewIntro">
+<h2>Ratings & Review</h2>
+<i class="far fa-user"></i>
+<p>What do you think?</p>
+<div class="text_rviewIntro">
+<div>
+<div id="rating-stars">
+<span class="star " onclick="rate(1)">&#9733;</span>
+<span class="star " onclick="rate(2)">&#9733;</span>
+<span class="star" onclick="rate(3)">&#9733;</span>
+<span class="star " onclick="rate(4)">&#9733;</span>
+<span class="star" onclick="rate(5)">&#9733;</span>
+
+</div><p >Rate book</p>
+</div>
+<button class="btn_rviewIntro">Write a Review</button>
+</div>
+
+</section>
+<hr>
+
+<section class="div1 Textrview">
+<div style='padding-top: 30px;'>
+<i class="far fa-user"></i>
+<p>Name</p>
+</div>
+<div class="textBok" >
+<div style="padding-bottom: 50px ;">
+<div id="rating-stars" style="width: 75%;float:left">
+<span class="star selected" onclick="rate(1)">&#9733;</span>
+<span class="star selected " onclick="rate(2)">&#9733;</span>
+<span class="star selected" onclick="rate(3)">&#9733;</span>
+<span class="star selected " onclick="rate(4)">&#9733;</span>
+<span class="star" onclick="rate(5)">&#9733;</span>
+</div>
+<div style="width: 25%;float:right;padding-top: 10px;">
+<p>April 2, 2020</p>
+</div>
+</div>
+
+<p  style="width:90%">Considering that The Lord of the Rings is one of the most popular books of the last century, it's surprising to see how
+few reviews there are here. I get the impression that many people feel guilty about liking it. It's a phase you go
+through, and the less said about it, the better. I think this is unfair to the book, which, I am prepared</p>
+<a href="'">Show more</a>
+<i class="fas fa-chevron-down"></i>
+</div>
+</section>
+`;
+
+bookInfo.appendChild(bookInfoItem);
+
+}
+
+
+getBooks();
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# BookApp" 
+"# Left to do"
+
+```
+- Review star can't be found in data
+- Do reviews (where does data come from or should we create them)
+- connect singlebook page to homepage. When you click books from homepage it will take you to singlebook page
+- Do the search function
+
+```
